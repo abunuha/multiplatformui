b3baf6de63bff26ffd4b48ce177a577b5a7c636c
Mohammed Malik <m_abdul_malik@yahoo.com>
2023-06-10 03:51:44 -0400

    Multiple tabs, Alhamdulillah

---
+++ MoonDate/MoonDate/History.cs
@@ -0,0 +1,15 @@
+namespace MoonDate;
+
+public class History : ContentPage
+{
+	public History()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
